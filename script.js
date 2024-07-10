b685bf41dd93b634bae52e42d871cc0c8fa5ba95
Kelvino7 <140096363+Kelvino7@users.noreply.github.com>
2024-07-10 21:49:28 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,197 @@
+const items = [
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/originals/0e/b5/d4/0eb5d40de268f48c828dd5c00700ea80.jpg",
+        "imageProfileImage": "C63.jpg",
+        "profileName": "Manuel",
+        "suggestedCategory": "fashion",
+        "title": "Stylish Outfit"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120003",
+        "imageUrl": "https://i.pinimg.com/564x/ce/fe/58/cefe589308c6c1ad0368236fb75004dc.jpg",
+        "imageProfileImage": "clock.png",
+        "profileName": "Gojo Satoru",
+        "suggestedCategory": "Anime",
+        "title": "JJK"
+    },
+    {
+        "id": "3c4d5e6f-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/736x/b1/02/ce/b102ce05dfdbf725d2bf69c1a2077e4b.jpg",
+        "imageProfileImage": "heart.webp",
+        "profileName": "Jason",
+        "suggestedCategory": "Art",
+        "title": "Stylish"
+    },
+    {
+        "id": "4d5e6f7g-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/66/42/a1/6642a1b84ab68aff5c5a1afa76f9d7be.jpg",
+        "imageProfileImage": "profile4.jpg",
+        "profileName": "Miles Morales",
+        "suggestedCategory": "MCU",
+        "title": "Spiderman"
+    },
+    {
+        "id": "5e6f7g8h-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/8d/b7/76/8db776bf2f32170ef1dcaf45197dd775.jpg",
+        "imageProfileImage": "profile5.jpg",
+        "profileName": "W204",
+        "suggestedCategory": "Cars",
+        "title": "Best AMG"
+    },
+    {
+        "id": "6f7g8h9i-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/736x/38/c3/1a/38c31a6305ea7ae4dacf5016676df36a.jpg",
+        "imageProfileImage": "profile6.jpg",
+        "profileName": "Ghost",
+        "suggestedCategory": "Video Games",
+        "title": "Call of Duty"
+    },
+    {
+        "id": "7g8h9i0j-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/20/7e/97/207e9789b72dce409efe4609e576ea3e.jpg",
+        "imageProfileImage": "profile7.jpg",
+        "profileName": "M4",
+        "suggestedCategory": "Cars",
+        "title": "BMW"
+    },
+    {
+        "id": "8h9i0j1k-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/bc/cd/79/bccd79ad8301e629e060902c19208aca.jpg",
+        "imageProfileImage": "profile8.jpg",
+        "profileName": "CJ",
+        "suggestedCategory": "Video games",
+        "title": "GTA San Andreas"
+    },
+    {
+        "id": "9i0j1k2l-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/e8/34/40/e834401953ee2ca6d5dd3828bc8e282d.jpg",
+        "imageProfileImage": "profile9.jpg",
+        "profileName": "Real Steel",
+        "suggestedCategory": "Movie",
+        "title": "Boxing"
+    },
+    {
+        "id": "0j1k2l3m-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/6b/61/7e/6b617eda8097d83a3e20d46fbb6fee46.jpg",
+        "imageProfileImage": "profile10.jpg",
+        "profileName": "Playstation",
+        "suggestedCategory": "Gaming",
+        "title": "PS4"
+    },
+    {
+        "id": "1k2l3m4n-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/ce/4b/57/ce4b573d0f130c205217d607c3b8e81f.jpg",
+        "imageProfileImage": "profile11.jpg",
+        "profileName": "Ford",
+        "suggestedCategory": "Cars",
+        "title": "Mustang 1969"
+    },
+    {
+        "id": "2l3m4n5o-1f0a-11ee-be56-0242ac120002",
+        "imageUrl": "https://i.pinimg.com/564x/6c/64/5d/6c645d589cea1abf31372916f20abe4b.jpg",
+        "imageProfileImage": "profile12.jpg",
+        "profileName": "Black Gru",
+        "suggestedCategory": "Meme",
+        "title": "Despicable Me"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120004",
+        "imageUrl": "https://i.pinimg.com/736x/37/67/dc/3767dcf11dc9976f1059d1de99a5750d.jpg",
+        "imageProfileImage": "exampleimage2.jpg",
+        "profileName": "Rick",
+        "suggestedCategory": "Series",
+        "title": "Rick & Morty"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120005",
+        "imageUrl": "https://i.pinimg.com/736x/f5/9f/8c/f59f8c5ca68ec3e3e3faa337736ae67a.jpg",
+        "imageProfileImage": "exampleimage3.jpg",
+        "profileName": "Kratos",
+        "suggestedCategory": "Gaming",
+        "title": "God of War"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120006",
+        "imageUrl": "https://i.pinimg.com/564x/3f/0c/e6/3f0ce6b8441f18e501e4cb9ff20ebbdd.jpg",
+        "imageProfileImage": "exampleimage4.jpg",
+        "profileName": "Batman",
+        "suggestedCategory": "DC",
+        "title": "Batman"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120007",
+        "imageUrl": "https://i.pinimg.com/564x/20/8f/f0/208ff0cd59054378ef8511744e96dce8.jpg",
+        "imageProfileImage": "exampleimage5.jpg",
+        "profileName": "Goku",
+        "suggestedCategory": "Anime",
+        "title": "Morning Yoga"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120008",
+        "imageUrl": "https://i.pinimg.com/564x/2c/e4/00/2ce400a74db46b109a8e111a5a25bc77.jpg",
+        "imageProfileImage": "exampleimage6.jpg",
+        "profileName": "Levi",
+        "suggestedCategory": "Anime",
+        "title": "Attack on Titan"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120009",
+        "imageUrl": "	https://i.pinimg.com/564x/90/5a/84/905a84363d102dc96963f53f2e8fa68e.jpg",
+        "imageProfileImage": "exampleimage7.jpg",
+        "profileName": "Moonknight",
+        "suggestedCategory": "MCU",
+        "title": "Moon Knight"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120010",
+        "imageUrl": "https://i.pinimg.com/564x/6f/59/8c/6f598cb86ff0abcf13124ad32467bf23.jpg",
+        "imageProfileImage": "exampleimage8.jpg",
+        "profileName": "Mark",
+        "suggestedCategory": "Series",
+        "title": "Invincible"
+    },
+    {
+        "id": "9b24be2c-1f0a-11ee-be56-0242ac120011",
+        "imageUrl": "https://i.pinimg.com/564x/2f/30/2a/2f302a7b020b78845c65b9ff90c58920.jpg",
+        "imageProfileImage": "exampleimage9.jpg",
+        "profileName": "Kanoe Rank",
+        "suggestedCategory": "Anime",
+        "title": "Demon Slayer"
+    },
+    
+];
+
+function generateItems(items) {
+    const container = document.getElementById('item-container');
+    container.innerHTML = '';
+    items.forEach(item => {
+        const itemDiv = document.createElement('div');
+        itemDiv.className = 'item';
+        itemDiv.innerHTML = `
+        <img src="${item.imageUrl}" alt="${item.title}">
+        <div class="item-details">
+            <div class="icon-container">
+                <i class="fas fa-share-alt share-icon"></i>
+                <i class="fas fa-ellipsis-v options-icon"></i>
+            </div>
+            <button class="save-button">Save</button>
+        </div>
+        `;
+        container.appendChild(itemDiv);
+    });
+}
+
+function filterItems(event) {
+    const query = event.target.value.toLowerCase();
+    const filteredItems = items.filter(item => 
+        item.title.toLowerCase().includes(query) || 
+        item.profileName.toLowerCase().includes(query)
+    );
+    generateItems(filteredItems);
+}
+
+document.getElementById('search-bar').addEventListener('input', filterItems);
+
+
+generateItems(items);
